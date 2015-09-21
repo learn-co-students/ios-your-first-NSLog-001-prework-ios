7b19abbe34e4acc2240602b1119adab99289366f
markedwardmurray <markedwardmurray@gmail.com>
2015-09-21 15:33:23 -0400

    style updates

--- your-first-NSLog/FISAppDelegate.h
+++ your-first-NSLog/FISAppDelegate.h
@@ -1,17 +1,10 @@
-//
-//  AppDelegate.h
-//  your-first-NSLog
-//
-//  Created by Tom OMalley on 5/14/15.
-//  Copyright (c) 2015 Flatiron School. All rights reserved.
-//
+//  FISAppDelegate.h
 
 #import <UIKit/UIKit.h>
 
 @interface FISAppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
-
 
 @end
 
